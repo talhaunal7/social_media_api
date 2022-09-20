fa11ac5266793d2180a6ffa9e16ced97a577299e
talhaunal7 <talhaunal7@gmail.com>
2022-09-20 14:39:28 +0300

    added login and register features

--- src/Routes/authenticate.js
+++ src/Routes/authenticate.js
@@ -2,12 +2,13 @@
 require('dotenv').config();
 const router = require("express").Router();
 const User = require("../Models/User");
-var CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js");
+const jwt = require('jsonwebtoken');
 
 
 router.post("/register", async (req, res) => {
     try {
-        const encryptedPassword = await CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString();
+        const encryptedPassword = await CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SECRET).toString();
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
@@ -16,26 +17,28 @@ router.post("/register", async (req, res) => {
         await newUser.save();
         res.status(200).json("successfully saved");
     } catch (error) {
-        console.log(error)
         res.status(500).json(error);
     }
 })
-//login with username and password
+
 router.post("/login", async (req, res) => {
-
     try {
-        const user=await User.findOne({username:req.body.username});
+        const user = await User.findOne({ username: req.body.username });
+        if (!user)
+            return res.status(404).json("invalid username");
 
-        !user && res.status(404).json("invalid username");
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
-        
+        if (originalPassword !== req.body.password)
+            return res.status(401).json("invalid password");
 
+        const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '45s' })
 
-
+        res.status(200).json({ accessToken });
     } catch (error) {
         res.status(500).json(error);
     }
-
 })
 
 module.exports = router
